7fd983b05f6561ad9989dd0ea06961c9063aac2c
olamedia <olamedia@gmail.com>
2012-03-09 16:35:27 +0600

    License

--- nokogiri.php
+++ nokogiri.php
@@ -1,5 +1,15 @@
 <?php
 
+/*
+ * This file is part of the zero package.
+ * Copyright (c) 2012 olamedia <olamedia@gmail.com>
+ *
+ * This source code is release under the MIT License.
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 /**
  * Simple HTML parser
  *
