cc20cc84200b2881647d777c455172b08c908ec5
olamedia <olamedia@gmail.com>
2013-01-21 15:25:11 +0600

    Fixed typo

--- nokogiri.php
+++ nokogiri.php
@@ -72,15 +72,15 @@ class nokogiri implements IteratorAggregate{
 		$dom->preserveWhiteSpace = false;
 		if (strlen($htmlString)){
 			libxml_use_internal_errors(true);
-			$doc->loadHTML('<?xml encoding="UTF-8">'.$htmlString);
+			$dom->loadHTML('<?xml encoding="UTF-8">'.$htmlString);
 			// dirty fix
-			foreach ($doc->childNodes as $item){
+			foreach ($dom->childNodes as $item){
 			    if ($item->nodeType == XML_PI_NODE){
-			        $doc->removeChild($item); // remove hack
+			        $dom->removeChild($item); // remove hack
 			        break;
 			    }
 			}
-			$doc->encoding = 'UTF-8'; // insert proper
+			$dom->encoding = 'UTF-8'; // insert proper
 			libxml_clear_errors();
 		}
 		$this->loadDom($dom);
